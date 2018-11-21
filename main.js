47c92c576aad8602d99da6fb9973d9d35573074f
Noufal Valapra <noufal.valapra@fingent.com>
2018-11-21 18:36:12 +0530

    fix

--- main.js
+++ main.js
@@ -51,43 +51,15 @@ app.on('activate', function () {
   }
 })
 
-autoUpdater.requestHeaders = { "PRIVATE-TOKEN": "Personal access Token" };
-autoUpdater.autoDownload = true;
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
 
-autoUpdater.setFeedURL({
-    provider: "generic",
-    url: "https://gitlab.com/noufalvalapra/electron-quick-start/-/jobs/artifacts/master/raw/dist?job=build"
+// when the update has been downloaded and is ready to be installed, notify the BrowserWindow
+autoUpdater.on('update-downloaded', (info) => {
+  mainWindow.webContents.send('updateReady')
 });
 
-autoUpdater.on('checking-for-update', function () {
-    sendStatusToWindow('Checking for update...');
-});
-
-autoUpdater.on('update-available', function (info) {
-    sendStatusToWindow('Update available.');
-});
-
-autoUpdater.on('update-not-available', function (info) {
-    sendStatusToWindow('Update not available.');
-});
-
-autoUpdater.on('error', function (err) {
-    sendStatusToWindow('Error in auto-updater.');
-});
-
-autoUpdater.on('download-progress', function (progressObj) {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + parseInt(progressObj.percent) + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
-});
-
-autoUpdater.on('update-downloaded', function (info) {
-    sendStatusToWindow('Update downloaded; will install in 1 seconds');
-});
-
-autoUpdater.on('update-downloaded', function (info) {
-    setTimeout(function () {
-        autoUpdater.quitAndInstall();
-    }, 1000);
-});
+// when receiving a quitAndInstall signal, quit and install the new version ;)
+ipcMain.on("quitAndInstall", (event, arg) => {
+    autoUpdater.quitAndInstall();
+})
