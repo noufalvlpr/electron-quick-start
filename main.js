4be66034d776e93e393413bca2b5e55c211e6512
Noufal Valapra <noufal.valapra@fingent.com>
2018-11-21 18:51:05 +0530

    fix

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const {autoUpdater} = require("electron-updater")
+var log = require('electron-log')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
